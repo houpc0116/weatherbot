f9d605a41c84ba5b2b6bd1b3e155ee637cc6aa93
hou13 <hou16@hotmail.com>
2017-09-09 03:47:37 +0800

    weatherbot v1.0.2

--- index.js
+++ index.js
@@ -1,92 +1,86 @@
-/* 解析天氣資訊 */
-const request = require('request');
-const cheerio = require("cheerio");
-const line = require("node-line-bot-api");
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 8080;
-//init with auth
-/* line.init({
-  accessToken: '{YOUR_ACCESS_TOKEN}',
-  // (Optional) for webhook signature validation
-  channelSecret: '{YOUR_CHANNEL_SECRET}'
-}); */
-var url = {
-  基隆:'http://www.cwb.gov.tw/V7/observe/24real/Data/46694.htm',
-  台北:'http://www.cwb.gov.tw/V7/observe/24real/Data/46692.htm',
-  新北:'http://www.cwb.gov.tw/V7/observe/24real/Data/46688.htm',
-  桃園:'http://www.cwb.gov.tw/V7/observe/24real/Data/C0C48.htm',
-  新竹:'http://www.cwb.gov.tw/V7/observe/24real/Data/46757.htm',
-  苗栗:'http://www.cwb.gov.tw/V7/observe/24real/Data/C0E75.htm',
-  台中:'http://www.cwb.gov.tw/V7/observe/24real/Data/46749.htm',
-  南投:'http://www.cwb.gov.tw/V7/observe/24real/Data/46765.htm',
-  彰化:'http://www.cwb.gov.tw/V7/observe/24real/Data/C0G66.htm',
-  雲林:'http://www.cwb.gov.tw/V7/observe/24real/Data/C0K33.htm',
-  嘉義:'http://www.cwb.gov.tw/V7/observe/24real/Data/46748.htm',
-  台南:'http://www.cwb.gov.tw/V7/observe/24real/Data/46741.htm',
-  高雄:'http://www.cwb.gov.tw/V7/observe/24real/Data/46744.htm',
-  屏東:'http://www.cwb.gov.tw/V7/observe/24real/Data/C0R22.htm',
-  宜蘭:'http://www.cwb.gov.tw/V7/observe/24real/Data/46708.htm',
-  花蓮:'http://www.cwb.gov.tw/V7/observe/24real/Data/46699.htm',
-  台東:'http://www.cwb.gov.tw/V7/observe/24real/Data/46766.htm',
-  綠島:'http://www.cwb.gov.tw/V7/observe/24real/Data/C0S73.htm',
-  蘭嶼:'http://www.cwb.gov.tw/V7/observe/24real/Data/46762.htm',
-  澎湖:'http://www.cwb.gov.tw/V7/observe/24real/Data/46735.htm',
-  金門:'http://www.cwb.gov.tw/V7/observe/24real/Data/46711.htm',
-  馬祖:'http://www.cwb.gov.tw/V7/observe/24real/Data/46799.htm',
-};
-
-for (var key in url) {
-  // Closure
-  (function(key){
-    request(url[key], function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        //console.log(body);
-        console.log(key);
-        var $ = cheerio.load(body);
-        var time = $('th').slice(11).eq(1).text();
-        console.log("觀測時間: "+time);
-        var temp = $('td').slice(0).eq(0).text();
-        console.log("現在溫度: "+temp);
-        var td_no = 2;
-        var weather = $('td').slice(td_no).eq(0).text();
-        var i = 10;
-        while (weather == 'X') {
-          weather = $('td').slice(td_no+i).eq(0).text();
-          td_no = td_no + i;
-        }
-        console.log("目前天氣: "+weather);  
-        console.log("------------------");
-      }
-    })
-
-  })(key);
-}
-/*
-1. 取得訊息內容, 判斷是哪一個區域
-2. 推播區域天氣資訊
+/* 
+   Linebot 
 */
-/*app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
-  // get content from request body
-  const promises = req.body.events.map(event => {
-    // reply message
-    return line.client
-      .replyMessage({
-        replyToken: event.replyToken,
-        messages: [
-          {
-            type: 'text',
-            text: event.message.text
-          }
-        ]
-      })
-  })
-  Promise
-    .all(promises)
-    .then(() => res.json({success: true}))
-}) */
-var server = app.listen(port, function () {
-  var host = server.address().address;
+const http = require('http');
+const request = require('request');
+const obj = require('./proc/areacode.js');   //自製JavaScript
+const linebot = require('linebot');
+const cheerio = require("cheerio");
+const express = require('express');
+const app = express();                 //const定義常數
+
+var bot = linebot({
+  channelId: "1506564035",
+  channelSecret: "86a7419c53e5fd640a840752b8cb26bc",
+  channelAccessToken: "nsz56i0Tt6y41jQzme1DfODzq+tnUUAiP9FWCy+ug+wET2vE0pLg7gJlFXv6uKZhIWXz6QcAaTQHsSY6mcS5uyiC3rjxqo8Yvwn+uby9cVXpNXpyQ3aNz736ej4b37kimyXP7ebWXfAWJ+/N0k5idAdB04t89/1O/w1cDnyilFU="
+});
+
+//當user有加入BOT的時候, JOIN是加入群組，Follow是加入好友
+bot.on('follow', function(event) {
+  console.log(event.source.userId); //userId
+  bot.push(''+event.source.userId+'', '請輸入您的姓名?');   //Bot傳送訊息
+});
+
+//觸發訊息, reply message 回覆訊息
+bot.on('message', function(event) {
+  console.log(event.message.type);
+  if(event.message.type == 'text') {            //判斷是文字格式的
+    var msg = event.message.text;
+    var get_code = obj.areaCode(msg);           //查詢區域編號
+    //console.log(get_code);
+    var msg_info = "";
+    request("http://www.cwb.gov.tw/V7/observe/24real/Data/"+get_code+".htm", function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+        var time = $('th').slice(11).eq(1).text();  //時間
+        var content = $("tr td.temp1");
+        var temp_val = $(content[0]).text();  //溫度
+        msg_info = "觀測時間:"+time+", 目前溫度:"+temp_val;
+        //console.log(msg_info);
+        event.reply(msg_info).then(function(data) {
+          console.log(msg);  //success
+        }).catch(function(error) {
+          console.log('error'); //error 
+        });
+      }
+    });
+  }else if(event.message.type == 'image'){   //判斷是圖片格式的
+
+  }else if(event.message.type == 'video'){   //判斷是影片格式的
+
+  }else if(event.message.type == 'audio'){   //判斷是聲音格式的
+
+  }else if(event.message.type == 'location'){   //判斷是地點的
+    //console.log(event.message.address);
+    var msg = event.message.address;
+    var get_code = obj.areaCode(msg);           //查詢區域編號
+    var msg_info = "";
+    request("http://www.cwb.gov.tw/V7/observe/24real/Data/"+get_code+".htm", function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+        var time = $('th').slice(11).eq(1).text();  //時間
+        var content = $("tr td.temp1");
+        var temp_val = $(content[0]).text();  //溫度
+        msg_info = "觀測時間:"+time+", 目前溫度:"+temp_val;
+        //console.log(msg_info);
+        event.reply(msg_info).then(function(data) {
+          console.log(msg);  //success
+        }).catch(function(error) {
+          console.log('error'); //error 
+        });
+      }
+    });
+
+  }else if(event.message.type == 'sticker'){   //判斷是表情符號
+
+  }
+});
+
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+var port = process.env.PORT || 8080;
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = http.createServer(app).listen(port, function () {
   var port = server.address().port;
 });
